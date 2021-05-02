64ac0e8b6241960dee2e28600c0229f6585323b8
pogopaule <pogopaule@posteo.de>
2021-05-02 17:54:35 +0200

    remove duplicate entries

--- src/create_readme.py
+++ src/create_readme.py
@@ -24,12 +24,22 @@ def nest(seq, keys):
 def toc_link(link):
     return "#" + re.sub(r"[^a-zA-Z-]", "", link.lower().replace(" ", "-"))
 
+def composed(f, g):
+    return lambda x: f(g(x))
+
+def unique_by_country(l):
+    return {k['country']:k for k in l}.values()
+
+def remove_duplicate_country_entry(job):
+    job['geo'] = unique_by_country(job['geo'])
+    return job
 
 with open("data.yaml", "r") as stream:
     try:
         DATA = yaml.safe_load(stream)
 
-        DENORMALIZED_JOBS = flat_map(denormalize, DATA["jobs"])
+        DENORMALIZED_JOBS = flat_map(composed(denormalize, remove_duplicate_country_entry), DATA["jobs"])
+
         NESTED_JOBS = nest(DENORMALIZED_JOBS, ["country", "field"])
         FIELDS = NESTED_JOBS.keys()
         NESTED_JOBPORTALS = nest(DATA["jobportals"], ["country"])
