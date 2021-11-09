dd4d0c3f4944ab7865f74c6e8da1e454a80549b9
pogopaule <pogopaule@posteo.de>
2021-11-09 17:44:01 +0100

    reformat code

--- src/create_readme.py
+++ src/create_readme.py
@@ -25,15 +25,19 @@ def nest(seq, keys):
 def toc_link(link):
     return "#" + re.sub(r"[^a-zA-Z-]", "", link.lower().replace(" ", "-"))
 
+
 def composed(f, g):
     return lambda x: f(g(x))
 
+
 def unique_by_country(l):
-    return {k['country']:k for k in l}.values()
+    return {k['country']: k for k in l}.values()
+
 
 def remove_duplicate_country_entry(job):
     job['geo'] = unique_by_country(job['geo'])
     return job
+
 
 with open("data.yaml", "r") as stream:
     try:
