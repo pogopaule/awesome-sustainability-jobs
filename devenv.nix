e941bfcdfd50056aec43da1e1be0bd297640510a
Fabian Schwarzer <fabian.schwarzer@node.energy>
2024-03-08 16:13:36 +0100

    update github actions to run with devenv

--- devenv.nix
+++ devenv.nix
@@ -20,4 +20,23 @@
       '';
     };
   };
+
+  scripts = {
+    create_readme.exec = ''
+      pushd src > /dev/null
+      python create_readme.py
+      popd > /dev/null
+    '';
+    create_map_data.exec = ''
+      pushd src > /dev/null
+      python create_map_data.py
+      popd > /dev/null
+    '';
+    update_reviews.exec = ''
+      pushd src > /dev/null
+      python update_reviews.py
+      popd > /dev/null
+    '';
+    test.exec = "pytest src/test_all.py";
+  };
 }
