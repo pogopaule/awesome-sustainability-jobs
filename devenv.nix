efd38e3d0dcf2722198fd7d72ac14a76024aff46
Fabian Schwarzer <fabian.schwarzer@node.energy>
2024-03-08 17:16:36 +0100

    let workflows fail correctly

--- devenv.nix
+++ devenv.nix
@@ -23,18 +23,18 @@
 
   scripts = {
     create_readme.exec = ''
-      pushd src > /dev/null
-      python create_readme.py
+      pushd src > /dev/null &&
+      python create_readme.py &&
       popd > /dev/null
     '';
     create_map_data.exec = ''
-      pushd src > /dev/null
-      python create_map_data.py
+      pushd src > /dev/null &&
+      python create_map_data.py &&
       popd > /dev/null
     '';
     update_reviews.exec = ''
-      pushd src > /dev/null
-      python update_reviews.py
+      pushd src > /dev/null &&
+      python update_reviews.py &&
       popd > /dev/null
     '';
     run_tests.exec = "pytest src/test_all.py";
