787cf44cd54a127164bf15616ce11970c69b470b
Fabian Schwarzer <fabian.schwarzer@node.energy>
2025-05-01 21:10:20 +0200

    test better map

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import polars as pl
 import streamlit as st
 import yaml
 from rich import pretty
+import folium
+from streamlit_folium import st_folium
 
 pretty.install()
 
@@ -34,36 +36,45 @@ jobs = read_jobs_yaml()
 portals = read_portals_yaml()
 
 
-only_remote = st.checkbox("Only remote jobs", value=False)
-only_speculative = st.checkbox("Only speculative jobs", value=False)
+only_remote = st.selectbox(
+    "",
+    [True, False],
+    index=None,
+    placeholder="Filter remote column",
+    format_func=lambda o: "Only remote" if o else "Only non-remote",
+)
+only_speculative = st.selectbox(
+    "",
+    [True, False],
+    index=None,
+    placeholder="Filter speculative column",
+    format_func=lambda o: "Only speculative" if o else "Only non-speculative",
+)
+
 selected_countries = st.multiselect(
     "Select countries", options=jobs["country"].unique().sort().to_list()
 )
 
-jobs_for_table = (
-    jobs.select(
-        "name",
-        "country",
-        "website",
-        "jobs",
-        "remote",
-        "speculative",
-        "rating",
-        "review",
+jobs = jobs.filter(
+    (
+        (pl.col("speculative") == only_speculative)
+        if only_speculative is not None
+        else True
     )
-    .filter(
-        ((pl.col("speculative") == only_speculative) if only_speculative else True)
-        & ((pl.col("remote") == only_remote) if only_remote else True)
-        & (
-            (pl.col("country").is_in(selected_countries))
-            if selected_countries
-            else True
-        )
-    )
-    .unique()
+    & ((pl.col("remote") == only_remote) if only_remote is not None else True)
+    & ((pl.col("country").is_in(selected_countries)) if selected_countries else True)
 )
 
-jobs_for_map = jobs.filter(pl.col("lat").is_not_null() & pl.col("long").is_not_null())
+jobs_for_table = jobs.select(
+    "name",
+    "country",
+    "website",
+    "jobs",
+    "remote",
+    "speculative",
+    "rating",
+    "review",
+).unique()
 
 
 st.dataframe(
@@ -75,8 +86,20 @@ st.dataframe(
         "review": st.column_config.LinkColumn(label="Review Site", display_text="Link"),
     },
 )
-st.map(
-    jobs_for_map,
-    latitude="lat",
-    longitude="long",
-)
+# st.map(
+#     jobs_for_map,
+#     latitude="lat",
+#     longitude="long",
+# )
+
+jobs_for_map = jobs.filter(pl.col("lat").is_not_null() & pl.col("long").is_not_null())
+
+m = folium.Map()
+for row in jobs_for_map.iter_rows(named=True):
+    folium.Marker(
+        [row["lat"], row["long"]],
+        popup=row["name"],
+        tooltip=row["name"],
+    ).add_to(m)
+
+st_data = st_folium(m, use_container_width=True, returned_objects=[])
